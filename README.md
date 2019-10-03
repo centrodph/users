8b59e25bdf96d9a66a5960643366d951ab5bbf40
Gerardo Perrucci <centrodph@gmail.com>
2019-10-03 15:41:02 -0300

    docker

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Url: [https://backusers.herokuapp.com/](https://backusers.herokuapp.com/)
 [ ] Setup  basic expressJS that respond JSON
 [ ] Config `pg` and test the heroku DB
 [ ] Deploy to heroku
+
+
+
+## run the app
+
+1. yarn
+2. yarn build-ts
+3. yarn watch-node (dev) or yarn start
--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,23 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
+/**
+ * Setup body parser
+ */
+app.use(bodyParser.json());
+/**
+ * Dummy Json
+ */
+app.get("/json", (request, response) => {
+    response.send({
+        author: "Gerardo Perrucci",
+        email: "centrodph@gmail.com"
+    });
+});
+/**
+ * Dummy route
+ */
 app.get("/", (request, response) => {
     response.send("Express APP working");
 });
