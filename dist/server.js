46a1c6f6f5110de78efbe25c73069771290694d9
Gerardo Perrucci <centrodph@gmail.com>
2019-10-02 22:28:38 -0300

    fix deploy issues

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,8 @@
 const express = require('express');
-
-
 const app = express();
 const port = process.env.PORT || 5000;
-
-
-
 app.get("/", (request, response) => {
-  response.send("Express APP working");
+    response.send("Express APP working");
 });
-
 app.listen(port);
+//# sourceMappingURL=server.js.map
