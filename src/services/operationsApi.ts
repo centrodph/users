f0c883a096e3498a3572641026ba7928a180a7ca
Gerardo Perrucci <centrodph@gmail.com>
2019-10-10 23:13:47 -0300

    create operation

--- src/services/operationsApi.ts
+++ src/services/operationsApi.ts
@@ -1,4 +1,4 @@
-import { listOperations } from "../model/query";
+import { listOperations, insertOperation } from "../model/query";
 import { db } from "../model/setup";
 
 export const getOperations = async (request, response) => {
@@ -7,7 +7,11 @@ export const getOperations = async (request, response) => {
 };
 
 export const createOperation = async (request, response) => {
-  const { rows } = await db.query(listOperations());
-  response.send(rows);
+  try {
+    const result = await db.query(insertOperation(request.body));
+    console.log(result);
+    response.send(result);
+  } catch (error) {
+    response.status(500).send({ message: error.detail, error });
+  }
 };
-
